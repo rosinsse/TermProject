f8b4dddd9640dc55745d96cb46422b1c7f671419
rosinsse <rosinsse@shernet.sheridancollege.ca>
2023-03-28 13:05:35 -0400

    I'm Done

---
+++ src/termproject/Card.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package termproject;
+
+/**
+ *
+ * @author srosi
+ */
+
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
