e47649d1218b6a019c1d2a78dedff682e1516da6
rosinsse <123481248+rosinsse@users.noreply.github.com>
2023-04-22 17:52:01 -0400

    Update Card.java

--- src/termproject/Card.java
+++ src/termproject/Card.java
@@ -1,23 +1,50 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package termproject;
+
+package com.mycompany.termcardgame;
 
 /**
  *
- * @author srosi
+ * @author SebastianR
  */
+public class Card {
 
-public abstract class Card {
-    //default modifier for child classes
+    public enum Rank {
+        ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+        EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+        private final int value;
 
+        private Rank(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum Suit {
+        CLUBS, DIAMONDS, HEARTS, SPADES;
+    }
+
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return rank.getValue();
+    }
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
